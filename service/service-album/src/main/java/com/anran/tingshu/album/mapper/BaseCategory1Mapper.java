cd175195288926faeebe82410f4fbc1a38837f73
An_ran <15804292449@163.com>
2025-08-09 20:28:56 +0800

    主页分类信息查询：mybatis 映射实现首页分类查询

--- service/service-album/src/main/java/com/anran/tingshu/album/mapper/BaseCategory1Mapper.java
+++ service/service-album/src/main/java/com/anran/tingshu/album/mapper/BaseCategory1Mapper.java
@@ -1,11 +1,15 @@
 package com.anran.tingshu.album.mapper;
 
 import com.anran.tingshu.model.album.BaseCategory1;
+import com.anran.tingshu.vo.category.CategoryVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BaseCategory1Mapper extends BaseMapper<BaseCategory1> {
 
 
+    List<CategoryVo> getBaseCategoryList();
 }
--- service/service-album/src/main/java/com/anran/tingshu/album/service/impl/BaseCategoryServiceImpl.java
+++ service/service-album/src/main/java/com/anran/tingshu/album/service/impl/BaseCategoryServiceImpl.java
@@ -36,53 +36,58 @@ public class BaseCategoryServiceImpl extends ServiceImpl<BaseCategory1Mapper, Ba
 	private BaseCategoryViewMapper baseCategoryViewMapper;
 
 
+	@Override
+	public List<CategoryVo> getBaseCategoryList() {
+		return baseCategory1Mapper.getBaseCategoryList();
+	}
+
 	/**
 	 * 传统方式实现 3级 查询
 	 * @return
 	 */
-    @Override
-    public List<CategoryVo> getBaseCategoryList() {
-
-		List<CategoryVo> res = new ArrayList<>();
-
-		// 1. 查询所有的分类信息
-		List<BaseCategoryView> categoryViewList = baseCategoryViewMapper.selectList(null);
-		// 2. 封装数据（难点）
-
-		// 对 1 级分类进行分组
-		Map<Long, List<BaseCategoryView>> firstCategoryMap = categoryViewList.stream().collect(Collectors.groupingBy(BaseCategoryView::getCategory1Id));
-
-		for (Map.Entry<Long, List<BaseCategoryView>> entry : firstCategoryMap.entrySet()) {
-			CategoryVo firstCategoryVo = new CategoryVo();
-			firstCategoryVo.setId(entry.getKey());
-			firstCategoryVo.setName(entry.getValue().get(0).getCategory1Name());
-
-			List<CategoryVo> firstCategoryChildren = new ArrayList<>();
-			// 对 2 级分类进行分组
-			Map<Long, List<BaseCategoryView>> secondCategoryMap = entry.getValue().stream().collect(Collectors.groupingBy(BaseCategoryView::getCategory2Id));
-
-			for (Map.Entry<Long, List<BaseCategoryView>> secondEntry : secondCategoryMap.entrySet()) {
-				CategoryVo secondCategoryVo = new CategoryVo();
-				secondCategoryVo.setId(secondEntry.getKey());
-				secondCategoryVo.setName(secondEntry.getValue().get(0).getCategory2Name());
-
-				List<CategoryVo> secondCategoryChildren = new ArrayList<>();
-				// 3 级 不需要再分组是一一对应的
-				for (BaseCategoryView baseCategoryView : secondEntry.getValue()) {
-					CategoryVo thirdCategoryVo = new CategoryVo();
-					thirdCategoryVo.setId(baseCategoryView.getCategory3Id());
-					thirdCategoryVo.setName(baseCategoryView.getCategory3Name());
-					thirdCategoryVo.setCategoryChildren(null);
-					secondCategoryChildren.add(thirdCategoryVo);
-				}
-				secondCategoryVo.setCategoryChildren(secondCategoryChildren);
-				firstCategoryChildren.add(secondCategoryVo);
-			}
-			firstCategoryVo.setCategoryChildren(firstCategoryChildren);
-			res.add(firstCategoryVo);
-		}
-
-		// 3. 返回
-		return res;
-    }
+//    @Override
+//    public List<CategoryVo> getBaseCategoryList() {
+//
+//		List<CategoryVo> res = new ArrayList<>();
+//
+//		// 1. 查询所有的分类信息
+//		List<BaseCategoryView> categoryViewList = baseCategoryViewMapper.selectList(null);
+//		// 2. 封装数据（难点）
+//
+//		// 对 1 级分类进行分组
+//		Map<Long, List<BaseCategoryView>> firstCategoryMap = categoryViewList.stream().collect(Collectors.groupingBy(BaseCategoryView::getCategory1Id));
+//
+//		for (Map.Entry<Long, List<BaseCategoryView>> entry : firstCategoryMap.entrySet()) {
+//			CategoryVo firstCategoryVo = new CategoryVo();
+//			firstCategoryVo.setId(entry.getKey());
+//			firstCategoryVo.setName(entry.getValue().get(0).getCategory1Name());
+//
+//			List<CategoryVo> firstCategoryChildren = new ArrayList<>();
+//			// 对 2 级分类进行分组
+//			Map<Long, List<BaseCategoryView>> secondCategoryMap = entry.getValue().stream().collect(Collectors.groupingBy(BaseCategoryView::getCategory2Id));
+//
+//			for (Map.Entry<Long, List<BaseCategoryView>> secondEntry : secondCategoryMap.entrySet()) {
+//				CategoryVo secondCategoryVo = new CategoryVo();
+//				secondCategoryVo.setId(secondEntry.getKey());
+//				secondCategoryVo.setName(secondEntry.getValue().get(0).getCategory2Name());
+//
+//				List<CategoryVo> secondCategoryChildren = new ArrayList<>();
+//				// 3 级 不需要再分组是一一对应的
+//				for (BaseCategoryView baseCategoryView : secondEntry.getValue()) {
+//					CategoryVo thirdCategoryVo = new CategoryVo();
+//					thirdCategoryVo.setId(baseCategoryView.getCategory3Id());
+//					thirdCategoryVo.setName(baseCategoryView.getCategory3Name());
+//					thirdCategoryVo.setCategoryChildren(null);
+//					secondCategoryChildren.add(thirdCategoryVo);
+//				}
+//				secondCategoryVo.setCategoryChildren(secondCategoryChildren);
+//				firstCategoryChildren.add(secondCategoryVo);
+//			}
+//			firstCategoryVo.setCategoryChildren(firstCategoryChildren);
+//			res.add(firstCategoryVo);
+//		}
+//
+//		// 3. 返回
+//		return res;
+//    }
 }

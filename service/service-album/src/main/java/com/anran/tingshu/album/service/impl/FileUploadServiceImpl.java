486ec6834889fb1a6555ca279d894f69742a2e19
An_ran <15804292449@163.com>
2025-08-13 19:47:34 +0800

    文件上传优化：上传文件去重操作

--- service/service-album/src/main/java/com/anran/tingshu/album/service/impl/FileUploadServiceImpl.java
+++ service/service-album/src/main/java/com/anran/tingshu/album/service/impl/FileUploadServiceImpl.java
@@ -3,8 +3,9 @@ package com.anran.tingshu.album.service.impl;
 import com.anran.tingshu.album.config.MinioProperties;
 import com.anran.tingshu.album.service.FileUploadService;
 import com.anran.tingshu.common.execption.BusinessException;
+import com.anran.tingshu.common.util.MD5;
 import io.minio.*;
-import io.minio.errors.MinioException;
+import io.minio.errors.*;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,22 +27,51 @@ public class FileUploadServiceImpl implements FileUploadService {
 
     @Override
     public String fileUpload(MultipartFile file) {
-        try {
-            // 1. 上传文件
-            PutObjectArgs.Builder putObjectArgsBuilder = PutObjectArgs.builder();
-            PutObjectArgs putObjectArgs = putObjectArgsBuilder
-                    .bucket(minioProperties.getBucketName())
-                    .object(file.getOriginalFilename())
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .build();
-            minioClient.putObject(putObjectArgs);
-            log.info("上传文件到 minio 成功");
 
-            // 返回图片 url
-            return minioProperties.getEndpointUrl() + "/" + minioProperties.getBucketName() + "/" + file.getOriginalFilename();
+        String picObject = "";
+
+        try {
+            // 1. 将文件按照文件内容加密，得到不同的文件名
+            byte[] bytes = file.getBytes();
+            String content = new String(bytes);
+            String prefix = MD5.encrypt(content);
+            String originalFilename = file.getOriginalFilename();
+            String suffix = originalFilename.substring(originalFilename.lastIndexOf(".", originalFilename.length()));
+            picObject = prefix + suffix;
+
+            // 2. Minio 判断是否存在该文件
+            StatObjectArgs.Builder builder = StatObjectArgs.builder();
+            StatObjectArgs statObjectArgs = builder
+                    .bucket(minioProperties.getBucketName())
+                    .object(picObject)
+                    .build();
+
+            // 逻辑是相反的，如果不抛异常说明存在
+            minioClient.statObject(statObjectArgs);
+            return minioProperties.getEndpointUrl() + "/" + minioProperties.getBucketName() + "/" + picObject;
+
+        } catch (IOException e) {
+            log.error("上传的文件不存在，原因：" + e.getMessage());
+            throw new BusinessException(201, "该文件不存在");
         } catch (Exception e) {
-            log.error("Minio 上传错误：" + e.getMessage());
-            throw new BusinessException(201, "上传文件到 minio 失败");
+
+            log.info("桶中不存在该对象，可以上传到 minio 中");
+            try {
+                // 3. 上传文件
+                PutObjectArgs.Builder putObjectArgsBuilder = PutObjectArgs.builder();
+                PutObjectArgs putObjectArgs = putObjectArgsBuilder
+                        .bucket(minioProperties.getBucketName())
+                        .object(picObject)
+                        .stream(file.getInputStream(), file.getSize(), -1)
+                        .build();
+                minioClient.putObject(putObjectArgs);
+                log.info("上传文件到 minio 成功");
+
+                return minioProperties.getEndpointUrl() + "/" + minioProperties.getBucketName() + "/" + picObject;
+            } catch (Exception ex) {
+                log.error("Minio 上传错误：" + ex.getMessage());
+                throw new BusinessException(201, "上传文件到 minio 失败");
+            }
         }
     }
 }
